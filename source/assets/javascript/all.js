6460ff63b056229dea6bb3e34f92d871fd8fc652
Alejandro GX <alex@flyingpigs.es>
2018-04-20 14:33:54 +0200

    Init commit

---
+++ source/assets/javascript/all.js
@@ -0,0 +1,21 @@
+var Project = {};
+
+Project.init = function(){
+  
+};
+
+$(document).on("ready", function(){
+  Project.init();
+});
+
+$(window).on("load", function(){
+
+});
+
+$(window).on("resize", function(){
+ 
+});
+
+$(window).on("scroll", function(){
+
+});
