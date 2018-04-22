e5f76c2e06e8025ff381766424789f2de2c84817
María Peña Jaquete <maria.pena.jaquete@gmail.com>
2018-04-22 12:22:03 +0200

    header and class ‘header—prueba-visible’ until we have definitive background

--- source/assets/javascript/all.js
+++ source/assets/javascript/all.js
@@ -17,5 +17,14 @@ $(window).on("resize", function(){
 });
 
 $(window).on("scroll", function(){
+  var scrollY = $(this).scrollTop();
 
+  var header = $('.header');
+  var headerOffset = 0;
+  if (scrollY > headerOffset && !header.hasClass('header--sticky')){
+    header.addClass('header--sticky');
+  }
+  else if(scrollY <= headerOffset && header.hasClass('header--sticky')) {
+    header.removeClass('header--sticky');
+  }
 });
