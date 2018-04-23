57617928489a36ff8e7ac8a77b51acd54cbc4892
María Peña Jaquete <maria.pena.jaquete@gmail.com>
2018-04-23 12:48:29 +0200

    footer mobile

--- source/assets/javascript/all.js
+++ source/assets/javascript/all.js
@@ -10,10 +10,50 @@ $(document).on("ready", function(){
 
 $(window).on("load", function(){
 
+  if ( $('.footer').length > 0 ) {
+
+    if ( $(window).width() < 768 ) {
+
+      $('.footer__title').on('click', function(ev) {
+        if ( $(this).is('.open') ) {
+          $(this).removeClass('open');
+          $(this).parent().find('.footer__nav').slideUp();
+        } else {
+          $(this).addClass('open');
+          $(this).parent().find('.footer__nav').slideDown();
+        }
+
+      });
+    }
+  }
 });
 
 $(window).on("resize", function(){
- 
+  if ( $(window).width() < 768 ) {
+    if ( $('.footer').length > 0 ) {
+
+      if ( $(window).width() < 768 ) {
+
+        $('.footer__title').on('click', function(ev) {
+          if ( $(this).is('.open') ) {
+            $(this).removeClass('open');
+            $(this).parent().find('.footer__nav').slideUp();
+          } else {
+            $(this).addClass('open');
+            $(this).parent().find('.footer__nav').slideDown();
+          }
+
+        });
+      }
+    }
+  }
+  else {
+    if ( $('.footer').length > 0 ) {
+      $('.footer__title').unbind('click');
+      $('.footer__nav').removeAttr('style');
+      $('.footer__title').removeClass('open');
+    }
+  }
 });
 
 $(window).on("scroll", function(){
